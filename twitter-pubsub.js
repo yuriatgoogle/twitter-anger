b591351c127aab244cd3514cd019afcdbb148f9e
Yuri Grinshteyn <ygrinshteyn@google.com>
2017-09-28 16:57:36 -0700

    added pubsub

--- twitter-pubsub.js
+++ twitter-pubsub.js
@@ -1,16 +1,33 @@
 //Twitter credentials in separate config file
-var config = require('./config.json');
+var twitterCreds = require('./twitterCreds.json');
 
+//twitter setup
 var Twit = require('twit');
-var T = new Twit(config); 
+var T = new Twit(twitterCreds); 
 
-
+//twitter parameters
 var params = {
     track: 'trump'
 } 
 
+//pubsub setup
+var pubsub = require('@google-cloud/pubsub')({
+    //specify correct project ID
+    projectId: 'ymg-twitter-node-dataflow-bq',
+    keyFilename: 'key.json'
+  });
+var topic = pubsub.topic('tweets');
+var publisher = topic.publisher();
+
 var stream = T.stream('statuses/filter', params)
 
+//start streaming tweets in and publishing them to PubSub
 stream.on('tweet', function (tweet) {
- console.log("tweet!")
+    console.log("got tweet!")
+    publisher.publish(new Buffer(JSON.stringify(tweet)), function(err, messageId) {
+        //if can't publish - 
+        function onError(err) {
+            console.log("error publishing - " + err);
+        }
+    });
 })
