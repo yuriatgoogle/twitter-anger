1f14b09efc018f8e22c15e815e43ecd99645f5b2
Yuri Grinshteyn <ygrinshteyn@google.com>
2017-09-28 13:55:41 -0700

    abstracted twitter secrets

--- twitter-pubsub.js
+++ twitter-pubsub.js
@@ -1,21 +1,15 @@
 //Twitter credentials
-var twitterConfig = {
-    consumer_key: 'EYcFK0zkD3LTuBzJhvKrAlY53',
-    consumer_secret: '7vnKYJnDMkBcHlw97C2abcV07UjzFlxnCrdlUsoyLE7XakueNq',
-    access_token: '1160099912-Pc3QPI32pyVJNnySqLD4xfOxIpLMWf6QiaIWOwY',
-    access_token_secret: 'BQbI3168xP4CleJncpbKFLZeSBJrdAqHx6GG6II9yvijv'
-}
+var config = require('./config.json');
 
-var Twit = require('twit'); // this is how we import the twit package
-var T = new Twit(twitterConfig); 
+var Twit = require('twit');
+var T = new Twit(config); 
 
 
 var params = {
-    q: 'donnie',
-    count: 100
+    track: 'trump'
 } 
 
-var stream = T.stream('statuses/sample')
+var stream = T.stream('statuses/filter', params)
 
 stream.on('tweet', function (tweet) {
  console.log("tweet!")
