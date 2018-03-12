88fc98f362a0b2a4158a40ed619e7b0bab945f47
Yuri Grinshteyn <ygrinshten@google.com>
2018-03-12 14:00:04 -0700

    Removing pub/sub references for local testing

--- twitter-pubsub.js
+++ twitter-pubsub.js
@@ -7,27 +7,12 @@ var T = new Twit(twitterCreds);
 
 //twitter parameters
 var params = {
-    user: 'ygrinshteyn'
+    user: 'yurigrinshteyn'
 }
-
-//pubsub setup
-var pubsub = require('@google-cloud/pubsub')({
-    //specify correct project ID
-    projectId: 'ymg-twitter-node-dataflow-bq',
-    keyFilename: 'key.json'
-  });
-var topic = pubsub.topic('tweets');
-var publisher = topic.publisher();
 
 var stream = T.stream('statuses/filter', params)
 
 //start streaming tweets in and publishing them to PubSub
 stream.on('tweet', function (tweet) {
     console.log("got tweet!")
-    publisher.publish(new Buffer(JSON.stringify(tweet)), function(err, messageId) {
-        //if can't publish -
-        function onError(err) {
-            console.log("error publishing - " + err);
-        }
-    });
 })
